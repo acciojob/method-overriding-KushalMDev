f20eb235de9ad43835c645e4c119e107dee70ccf
KushalMDev <kushm1929@gmail.com>
2023-12-19 15:15:00 +0530

    Final Commit

--- src/main/java/com/driver/A.java
+++ src/main/java/com/driver/A.java
@@ -1,6 +1,5 @@
 package com.driver; 
 public class A {
-   public A(){}
     public String meth(){
             return "Invoking method from class A";
     }
--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,10 +1,6 @@
 package com.driver;
 
 public class B extends A {
-   public B(){
-
-    }
-    
     @Override
     public String meth() {
         // TODO Auto-generated method stub
