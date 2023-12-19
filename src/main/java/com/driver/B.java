e65843f93003782cfb349f5af8804406dcd0c177
KushalMDev <kushm1929@gmail.com>
2023-12-19 15:09:47 +0530

    Third Commit

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 public class B extends A {
-    B(){
+   public B(){
 
     }
     
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-    // B b=new B();
-    // System.out.println(b.meth());
+    B b=new B();
+    System.out.println(b.meth());
   }
 }
