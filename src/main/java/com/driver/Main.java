707e5ff29b449a558b98648ee2bed6979362233d
KushalMDev <kushm1929@gmail.com>
2023-12-20 16:19:59 +0530

    final commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,19 @@
 package com.driver;
 
 public class Main {
+  public static class A {
+    public String meth(){
+            return "Invoking method from class A";
+    }
+}
+public static class B extends A {
+  @Override
+  public String meth() {
+      // TODO Auto-generated method stub
+      return "Method is overridden in Extendend class B";
+  }
+}
+
   public static void main(String[] args) {
     B b=new B();
     System.out.println(b.meth());
